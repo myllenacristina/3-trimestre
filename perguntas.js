b11b2011eb08a66085cd96572a7bedd6c366a25a
myllenacristina <myllenacristinanascimento2@gmail.com>
2024-10-31 09:42:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criacartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criacartao(
+"Programação",
+"O que é JavaScript?",
+"O JavaScript é uma linguagem de programação"
+)
+
+criacartao(
+    "geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criacartao(
+    "Programação",
+"O que é uma função?",
+"Uma função é um bloco de código que executa alguma tarefa"
+)
